fa9b072bb7c997d9311c5b229cc124b7f8f95f6b
Midi Nanakul <60481557+Nanakul@users.noreply.github.com>
2020-01-30 13:59:39 -0800

    Add files via upload
    Guess a number between 1 and 100. You have FIVE attempts to guess! :)

---
+++ GuessingGame.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    Scanner console = new Scanner(System.in);
+
+    int attemptsNum = 0;
+    final int maxAttempts = 5;
+    int random = 1 + (int) (Math.random() * 99);
+    int guess = 0;
+
+    System.out.println("I'm thinking of a number between 1 and 100. Can you guess it in less than 5 tries?");
+
+    while(guess != random && attemptsNum < maxAttempts) {
+      guess = console.nextInt();
+      attemptsNum++;
+
+      if(guess > random) {
+        System.out.println("Gotta think lower!");
+      } else if (guess < random) {
+        System.out.println("Gotta think higher!");
+      } else {
+        System.out.println("Congratulations, you win! It took you " + attemptsNum + " attempt(s)");
+      }
+
+      if (attemptsNum == maxAttempts)
+        System.out.println("You lose! The number was: " + random);
+    }
+  }
+}
