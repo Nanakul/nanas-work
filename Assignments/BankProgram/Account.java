e89efa12c9b1eca2eef3500a5acab347a8a9a4ec
Midi Nanakul <60481557+Nanakul@users.noreply.github.com>
2020-02-21 13:58:48 -0800

    Updated Account.java
    Added comments

--- Assignments/BankProgram/Account.java
+++ Assignments/BankProgram/Account.java
@@ -1,14 +1,14 @@
-package week5;
-
 public class Account {
 	
+    //Set Parameters
 	private double balance;
 	private String accountNumber;
 	public static double MINIMUM_BAL = 0.00;
 	public static double OVERDRAFT_FEE = 25.00;
 	
+    //Create Constructors
 	public Account() {
-		balance = 0.00;
+		balance = MINIMUM_BAL;
 	}
 	
 	public Account(String accountNumber, double balance) {
@@ -33,6 +33,8 @@ public class Account {
 	  //Withdraw funds
 	  public void withdrawFunds(double amount) {
 		balance -= amount;
+
+          //Write an IF statement that issues an Overdraft fee if the client goes below a balance of 0.
 		if(balance < 0) {
 			balance -= OVERDRAFT_FEE;
 			System.out.println("You have been issued a $25 Overdraft Fee. If you would like to "
@@ -47,4 +49,4 @@ public class Account {
 	    System.out.println(balance);
 	  }
 	  
-	}
+}
